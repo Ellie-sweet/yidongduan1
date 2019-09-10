bf4f1f598f8913fe847cad2d0b37cb219eee07d3
Ellie <779537278@qq.com>
2019-09-10 20:45:13 +0800

    readme edit message

--- 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/common/Message.vue
+++ 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/common/Message.vue
@@ -1,65 +1,23 @@
 <template>
-  <div class="container">
-            <!-- 首页搜索框 -->
-            <div>
-              <img src="" alt=""><input type="text" placeholder="搜索多抓鱼">
-            </div>
-            <!-- 轮播图 -->
-            <div>
-              <img src="" alt="">
-            </div>
-            <!-- 分类 -->
-            <div>
-              <a href="">猜你喜欢</a>
-            </div>
-            <!-- 两个按钮分类 -->
-            <div>
-              <div class="btn">
-                <mt-button size="small" @click.native.prevent="active='all'">全部</mt-button>
-                <mt-button size="small" @click.native.prevent="active='rank'">综合排序</mt-button>
-              </div>
-              <div>      
-                <mt-tab-container v-model="active">
-                  <mt-tab-container-item id="all">
-                    <mt-cell title="图片"></mt-cell>
-                  </mt-tab-container-item>
-                  <mt-tab-container-item id="rank">
-                    <mt-cell></mt-cell>
-                  </mt-tab-container-item>
-                </mt-tab-container>
-              </div>
-            </div>
-            
-            <!-- 列出的豆瓣评分8.5分以上的书籍 -->
-            <div>
-              <!-- 左边图片与文字 -->
-              <div>
-                <img src="" alt="">
-              </div>
-              <!-- 右边小图标 -->
-              <div>
-                <img src="" alt="">
-              </div>
-            </div>
-            <!-- 分类和全部分类 -->
-            <div>
-              <span>去逛逛这些分类</span><a href="#">全部分类</a>
-              <div></div>
-            </div>
-            <!-- 推荐的书籍 -->
-            <div>
-              <img src="" alt="">
-            </div>    
-          </div>
+  <div>   
+    <!-- 轮播图 -->
+    <div>
+      <img src="../../../assets/chongban.png"> 
+    </div>
+  </div>
 </template>
 
 <script>
+// 引入01子组件
+
 export default {
   data(){
     return {
-      active:"all",
+     
     }
   },
+  
+  
 }
 </script>
 
--- 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/common/MessageList.vue
+++ 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/common/MessageList.vue
@@ -1,6 +1,62 @@
 <template>
   <div>
     <message></message>
+    <div class="container">
+            <!-- 首页搜索框 -->
+            <div>
+              <mt-search v-model="value" cancel-text="取消" placeholder="搜索"></mt-search>
+            </div>
+            <!-- 轮播图 -->
+            <div>
+              <mt-swipe :show-indicator="false">
+                <mt-swipe-item >
+                  <message></message>
+                </mt-swipe-item>
+              </mt-swipe>
+            </div>
+            <!-- 分类 -->
+            <div>
+              <a href="">猜你喜欢</a>
+            </div>
+            <!-- 两个按钮分类 -->
+            <div>
+              <div class="btn">
+                <mt-button size="small" @click.native.prevent="active='all'">全部</mt-button>
+                <mt-button size="small" @click.native.prevent="active='rank'">综合排序</mt-button>
+              </div>
+              <div>      
+                <mt-tab-container v-model="active">
+                  <mt-tab-container-item id="all">
+                    <mt-cell title="图片"></mt-cell>
+                  </mt-tab-container-item>
+                  <mt-tab-container-item id="rank">
+                    <mt-cell></mt-cell>
+                  </mt-tab-container-item>
+                </mt-tab-container>
+              </div>
+            </div>
+            
+            <!-- 列出的豆瓣评分8.5分以上的书籍 -->
+            <div>
+              <!-- 左边图片与文字 -->
+              <div>
+                <img src="" alt="">
+              </div>
+              <!-- 右边小图标 -->
+              <div>
+                <img src="" alt="">
+              </div>
+            </div>
+            <!-- 分类和全部分类 -->
+            <div>
+              <span>去逛逛这些分类</span><a href="#">全部分类</a>
+              <div></div>
+            </div>
+            <!-- 推荐的书籍 -->
+            <div>
+              <img src="" alt="">
+            </div>    
+          </div>
   </div>
 </template>
 
@@ -9,12 +65,18 @@
 import Message from "./Message"
 export default {
   data(){
-    return {}
+    return {
+      value:"",
+      active:"all",
+    }
+  },
+  methods:{
+   
   },
   // 注册子组件
   components:{
     "message":Message,
-  }
+  },
 }
 </script>
 
