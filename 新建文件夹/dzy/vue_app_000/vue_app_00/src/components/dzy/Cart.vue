3ceb62c5fc2716eaf59bf3e70ec841da1ef4db53
Ellie-sweet <52389247+Ellie-sweet@users.noreply.github.com>
2019-09-16 13:35:15 +0800

    message

--- 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/Cart.vue
+++ 新建文件夹/dzy/vue_app_000/vue_app_00/src/components/dzy/Cart.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
-    嘻嘻嘻嘻嘻
+    <div>
+      <!-- 页头 -->
+      <div>
+        <img src="">
+        <mt-button>登出账号</mt-button>
+      </div>
+      <!-- 购物小组件 -->
+      <div>
+        <div>
+          <img src="" alt="">
+          余额
+        </div>
+        <div>
+          <img src="" alt="">
+          余额订单
+        </div>
+        <div>
+          <img src="" alt="">
+          买卖的书
+        </div>
+        <div>
+          <img src="" alt="">
+          通知
+        </div>
+      </div>
+      <!-- 动态，书架 -->
+      <div>
+
+      </div>
+      <!-- 读书笔记 -->
+      <div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
